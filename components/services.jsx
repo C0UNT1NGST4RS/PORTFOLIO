5d4241e1a181c889c2e0761d587f8b10a0b6dcb9
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-07-08 16:22:10 +0530

    Update services.jsx

--- components/services.jsx
+++ components/services.jsx
@@ -13,7 +13,19 @@ function services() {
             <h3 className={style.h3}>Dev</h3>
           </div>
           <ul className={style.list}>
-            {/* Services under "Dev" */}
+            {/* Services under "Dev" */
+            <li className={style.li}><Done className={style.icon}/> Small Scale Automation in python</li>
+            <li className={style.li}><Done className={style.icon}/> Server Configuration</li>
+            <li className={style.li}><Done className={style.icon}/> Debugging</li>
+            <li className={style.li}><Done className={style.icon}/> Testing[Black box]</li>
+            <li className={style.li}><Done className={style.icon}/> Website Maintenance</li>
+            <li className={style.li}><Done className={style.icon}/> Static Website Service</li>
+            <li className={style.li}><Done className={style.icon}/> Network Configuration</li>
+            <li className={style.li}><Done className={style.icon}/> Database Management</li>
+            <li className={style.li}><Done className={style.icon}/> Web Hosting</li>
+            <li className={style.li}><Done className={style.icon}/> Payment Gatway Integration</li>
+            <li className={style.li}><Done className={style.icon}/> API Integrations</li>
+}
           </ul>
         </div>
 
@@ -22,61 +34,26 @@ function services() {
             <h3 className={style.h3}>OTHER SERVICES</h3>
           </div>
           <ul className={style.list}>
-            {/* Services under "OTHER SERVICES" */}
+            {/* Services under "OTHER SERVICES" */
+             <li className={style.li}><Done className={style.icon}/> Small Scale Automation in python</li>
+            <li className={style.li}><Done className={style.icon}/> Server Configuration</li>
+            <li className={style.li}><Done className={style.icon}/> Debugging</li>
+            <li className={style.li}><Done className={style.icon}/> Testing[Black box]</li>
+            <li className={style.li}><Done className={style.icon}/> Website Maintenance</li>
+            <li className={style.li}><Done className={style.icon}/> Static Website Service</li>
+            <li className={style.li}><Done className={style.icon}/> Network Configuration</li>
+            <li className={style.li}><Done className={style.icon}/> Database Management</li>
+            <li className={style.li}><Done className={style.icon}/> Web Hosting</li>
+            <li className={style.li}><Done className={style.icon}/> Payment Gatway Integration</li>
+            <li className={style.li}><Done className={style.icon}/> API Integrations</li>
+}
           </ul>
         </div>
       </div>
     </section>
 
-    
-    <section id='services'>
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
-
-      <div className={style.container}>
-        <article className={style.service}>
-          <div className={style.header}>
-            <h3 className={style.h3}>Dev</h3>
-          </div>
-          <ul className={style.list}>
-            <li className={style.li}><Done className={style.icon}/> Small Scale Automation in python</li>
-            <li className={style.li}><Done className={style.icon}/> Server Configuration</li>
-            <li className={style.li}><Done className={style.icon}/> Debugging</li>
-            <li className={style.li}><Done className={style.icon}/> Testing[Black box]</li>
-            <li className={style.li}><Done className={style.icon}/> Website Maintenance</li>
-            <li className={style.li}><Done className={style.icon}/> Static Website Service</li>
-            <li className={style.li}><Done className={style.icon}/> Network Configuration</li>
-            <li className={style.li}><Done className={style.icon}/> Database Management</li>
-            <li className={style.li}><Done className={style.icon}/> Web Hosting</li>
-            <li className={style.li}><Done className={style.icon}/> Payment Gatway Integration</li>
-            <li className={style.li}><Done className={style.icon}/> API Integrations</li>
-
-          </ul>
-        </article>
-      </div>
-      
-      <div className={style.container}>
-        <article className={style.service}>
-          <div className={style.header}>
-            <h3 className={style.h3}>Other Services</h3>
-          </div>
-          <ul className={style.list}>
-            <li className={style.li}><Done className={style.icon}/> Small Scale Automation in python</li>
-            <li className={style.li}><Done className={style.icon}/> Server Configuration</li>
-            <li className={style.li}><Done className={style.icon}/> Debugging</li>
-            <li className={style.li}><Done className={style.icon}/> Testing[Black box]</li>
-            <li className={style.li}><Done className={style.icon}/> Website Maintenance</li>
-            <li className={style.li}><Done className={style.icon}/> Static Website Service</li>
-            <li className={style.li}><Done className={style.icon}/> Network Configuration</li>
-            <li className={style.li}><Done className={style.icon}/> Database Management</li>
-            <li className={style.li}><Done className={style.icon}/> Web Hosting</li>
-            <li className={style.li}><Done className={style.icon}/> Payment Gatway Integration</li>
-            <li className={style.li}><Done className={style.icon}/> API Integrations</li>
-
-          </ul>
-        </article>
-      </div>
-    </section>
+   
+        
   )
 }
 
