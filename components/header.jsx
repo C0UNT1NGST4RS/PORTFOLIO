306b678f516d31bbfb93fe3b7add32d3be2856a7
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-07-08 15:11:28 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -10,8 +10,8 @@ function header() {
     <header className={style.header}>
       <div className={`container  ${style.container}`}>
         <h5>Hello  I&lsquo;m</h5>
-        <h1>Siyabuddeen</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h1>Abhishek Mathew</h1>
+        <h5 className="text-light">Software Developer | ML | Pentester | AWS | Networking | Python </h5>
         <div className={style.cta}>
           <a className="btn" href={'/assets/cv.pdf'} >Download CV</a>
           <a href={'#contact'}><span className="btn btn-primary">
@@ -21,9 +21,9 @@ function header() {
         <a href={'#contact'}className={style.scrollDown}>Scroll Down</a>
 
         <div className={style.socials}>
-          <Link href={'https://in.linkedin.com/in/mohd-shihab-442b681b2'}><LinkedInIcon/></Link>
-          <Link href={'https://github.com/mahammed-siyabuddeen/'}><GitHubIcon/></Link>
-          <Link href={'https://www.instagram.com/siyab._/'}><InstagramIcon/></Link>
+          <Link href={'https://in.linkedin.com/in/'}><LinkedInIcon/></Link>
+          <Link href={'https://github.com/el3ktraz/'}><GitHubIcon/></Link>
+          <Link href={'https://www.instagram.com/4bhiX_X/'}><InstagramIcon/></Link>
         </div>
       </div>
 
